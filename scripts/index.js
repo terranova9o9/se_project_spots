3c476d6e90a71910829e46c45b4c6d23937a03d2
Brandon Santos <nodnarbftw@gmail.com>
2024-08-13 09:42:57 -0700

    modal open and close script

--- scripts/index.js
+++ scripts/index.js
@@ -19,4 +19,19 @@ const initialCards = [
 },
 ];
 
-console.log (initialCards);
+
+const profileEditButton = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const modalCloseButton = document.querySelector(".modal__close-btn");
+
+function openModal () {
+  editProfileModal.classList.add ('modal_opened');
+}
+
+profileEditButton.addEventListener ('click', openModal);
+
+function closeModal () {
+  editProfileModal.classList.remove ('modal_opened');
+}
+
+modalCloseButton.addEventListener ('click', closeModal);
