48541ce9ffb4107adbb2f1005ef5fa35806114ad
Brandon Santos <nodnarbftw@gmail.com>
2024-08-13 15:09:32 -0700

    finished javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
 const initialCards = [
 {
-  name: "Val Thorens", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+  name: "Val Thorens", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg", alt:"Val Thorens"
 },
 {
-  name: "Restaurant terrace", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
+  name: "Restaurant terrace", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg" , alt:"restaurant"
 },
 {
-  name: "An outdoor cafe", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
+  name: "An outdoor cafe", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg", alt:"outdoor cafe"
 },
 {
-  name: "A very long bridge, over the forest and through the trees", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
+  name: "A very long bridge, over the forest and through the trees", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg", alt:"long bridge"
 },
 {
-  name: "Tunnel with morning light", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
+  name: "Tunnel with morning light", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg", alt:"tunnel"
 },
 {
-  name: "Mountain house", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
+  name: "Mountain house", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg", alt:"mountain house"
 },
 ];
 
@@ -31,6 +31,23 @@ const editModalNameInput = editProfileModal.querySelector("#profile-name-input")
 const editModalDescriptionInput = editProfileModal.querySelector("#profile-description-input");
 
 const editFormElement = editProfileModal.querySelector(".modal__form");
+
+const cardTemplate = document.querySelector("#card-template");
+const cardList = document.querySelector(".cards__list");
+const cardImage = document.querySelector(".card__image");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
+  const cardNameElement = cardElement.querySelector(".card__title");
+  const cardImageElement = cardElement.querySelector(".card__image");
+
+  cardNameElement.textContent = data.name;
+  cardImageElement.src = data.link;
+  cardImageElement.alt = data.alt;
+
+
+  return cardElement;
+};
 
 
 function openModal () {
@@ -53,7 +70,10 @@ profileEditButton.addEventListener ('click', openModal);
 modalCloseButton.addEventListener ('click', closeModal);
 editFormElement.addEventListener ('submit', handleEditFormSubmit);
 
-
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardList.append(cardElement);
+};
 
 
 
